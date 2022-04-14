af56014d48ea7af36d9646abb0055dd66999cc7c
EC2 Default User <ec2-user@ip-172-31-73-129.ec2.internal>
2022-04-14 12:09:14 +0000

    メモアプリ修正

---
+++ memo.rb
@@ -0,0 +1,39 @@
+require "csv"
+puts "1(新規でメモを作成) 2(既存のメモを編集する)"
+memo_type= gets.chomp
+
+if memo_type == "1"
+  puts "拡張子を除いたファイル名を入力してください"
+  memo_type = gets.chomp
+  CSV.open("#{memo_type}.csv","w") do | csv |
+    csv << ["1"]
+    csv << ["#{memo_type}"]
+  end
+    
+  str  = memo_type
+  puts "メモしたい内容を記入してください"
+  puts "完了後、Ctrl + Dを押します"
+  memo_type = $stdin.read
+  CSV.open("#{str}.csv","a") do | csv |
+    csv << ["#{memo_type}"]
+  end
+    
+elsif memo_type == "2"
+  puts "編集したいファイル名を入力してください"
+  memo_type= gets.chomp
+  CSV.open("#{memo_type}.csv","w") do | csv |
+    csv << ["2"]
+    csv << ["#{memo_type}"]
+  end
+    
+  str  = memo_type
+  puts "編集内容を記入してください"
+  puts "完了後、Ctrl + Dを押します"
+  memo_type = $stdin.read
+  CSV.open("#{str}.csv","a") do | csv |
+    csv << ["#{memo_type}"]
+  end
+    
+else 
+  puts "エラー"
+end
