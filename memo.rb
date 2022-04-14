6c6cdf747cf93581d5183b9a181689510561ac1b
EC2 Default User <ec2-user@ip-172-31-73-129.ec2.internal>
2022-04-14 22:34:18 +0000

    課題修正

--- memo.rb
+++ memo.rb
@@ -6,24 +6,20 @@ if memo_type == "1"
   puts "拡張子を除いたファイル名を入力してください"
   memo_type = gets.chomp
   CSV.open("#{memo_type}.csv","w") do | csv |
-    csv << ["1"]
-    csv << ["#{memo_type}"]
   end
     
   str  = memo_type
   puts "メモしたい内容を記入してください"
   puts "完了後、Ctrl + Dを押します"
   memo_type = $stdin.read
-  CSV.open("#{str}.csv","a") do | csv |
+  CSV.open("#{str}.csv","w") do | csv |
     csv << ["#{memo_type}"]
   end
     
 elsif memo_type == "2"
   puts "編集したいファイル名を入力してください"
   memo_type= gets.chomp
-  CSV.open("#{memo_type}.csv","w") do | csv |
-    csv << ["2"]
-    csv << ["#{memo_type}"]
+  CSV.open("#{memo_type}.csv","a") do | csv |
   end
     
   str  = memo_type
